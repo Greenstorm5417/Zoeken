fbef4faac966a30c07dd33aa6d4f3e4d619d62d5
Greenstorm5417 <130579625+Greenstorm5417@users.noreply.github.com>
2026-07-28 19:08:42 +0000

    chore: update nix generated.nix for v1.4.1

--- packaging/nix/generated.nix
+++ packaging/nix/generated.nix
@@ -1,14 +1,14 @@
 # Auto-updated from GitHub release assets (see tools/update_nix_generated.sh).
 {
-  version = "1.4.0";
+  version = "1.4.1";
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/Greenstorm5417/Zoeken/releases/download/v1.4.0/zoeken_1.4.0_x86_64-linux.tar.gz";
-      hash = "sha256-ccHqIkSSSVCMtWQ8D+i6+zXmVsckjklc4o35ojsRfp0=";
+      url = "https://github.com/Greenstorm5417/Zoeken/releases/download/v1.4.1/zoeken_1.4.1_x86_64-linux.tar.gz";
+      hash = "sha256-vNOiJxSjkyDlc+fZQd7lTBEjB/01QGYGBj+frBfBZP0=";
     };
     "aarch64-linux" = {
-      url = "https://github.com/Greenstorm5417/Zoeken/releases/download/v1.4.0/zoeken_1.4.0_aarch64-linux.tar.gz";
-      hash = "sha256-SQiQyXIC0nPf3ltXCLSdXIQJ9RoMQOip8IJJIx2EpDg=";
+      url = "https://github.com/Greenstorm5417/Zoeken/releases/download/v1.4.1/zoeken_1.4.1_aarch64-linux.tar.gz";
+      hash = "sha256-Ow2EaiQycDBpFv+NaoglgziMci6lIGl+i79H3mFLJOs=";
     };
   };
 }
